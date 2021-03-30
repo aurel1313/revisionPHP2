9c5f2af0fd1883f824ba1c680058f34ca81fcadb
Aurélien Fabre <fabrea@saintjo.org>
2021-03-30 15:18:35 +0200

    programmation objet

---
+++ voiture.php
@@ -0,0 +1,26 @@
+<?php
+    class Voiture{
+        private $marque;
+        private $couleur;
+        private $immatriculations;
+        
+        
+        public function getMarque(){
+            return $this->marque;
+        }
+        public function setMarque($marque2){
+            $this->marque =$marque2;
+        }
+        public function __construct($m,$c,$i){
+            $this->marque=$m;
+            $this->couleur=$c;
+            $this->immatriculation=$i;
+        }
+        public function afficher(){
+            echo $this->marque;
+        }
+        
+    }
+
+
+?>
